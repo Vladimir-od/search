038db77a2e99684e931a8d241dee90c2e75830e3
Kravtsov-Vladimir <58466406+Vladimir-od@users.noreply.github.com>
2020-01-23 12:34:44 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Container} from './container';
+import {SearchService} from './search.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SearchContainer';
+export class AppComponent implements OnInit {
+  container: Container;
+
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit() {
+    this.searchService.getContainer(this.container).subscribe(res => this.container = res);
+  }
+  getContainer(numberId) {
+    this.searchService.getContainer(numberId).subscribe(c => this.container = c);
+  //  console.log(this.container);
+  }
+  clear(form: NgForm) {
+    form.reset();
+  }
 }
+
