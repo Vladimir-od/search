56b9ee710732f7e41dad3ac6686179f1babd10bb
Kravtsov-Vladimir <58466406+Vladimir-od@users.noreply.github.com>
2020-01-23 12:42:51 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SearchService} from './search.service';
+import { ContainerDetailesComponent } from './container-detailes/container-detailes.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerDetailesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/container.ts
@@ -0,0 +1,12 @@
+export class Container {
+  id: string;
+  owner: string;
+}
+export class History {
+  dateFrom: number;
+  dateTo: number;
+  sourcePort: string;
+  destinationPort: string;
+  sender: string;
+  recipient: string;
+}
